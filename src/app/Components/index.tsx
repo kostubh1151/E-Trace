d9fc3b5ab43bc16cc8473d8f77bd758454690871
Kostubh <121478617+kostubh1151@users.noreply.github.com>
2025-05-04 15:53:56 +0530

    Update index.tsx

--- src/app/Components/index.tsx
+++ src/app/Components/index.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Hero from './Hero'
 import Features from './Features'
 import FAQ from './FAQ'
-
+import EWastePredictor from './EWastePredictor'
 type Props = {}
 
 const Home = (props: Props) => {
   return (
     <>
     <Hero/>
+      <EWastePredictor/>
     <Features/>
     <FAQ/>
     </>
